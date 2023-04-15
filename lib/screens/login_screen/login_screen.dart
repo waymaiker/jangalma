7ca7ca99570ec13b30934767ad10ddd31b2a2ae5
Bonkomprann <waymaiker@laposte.net>
2023-04-15 20:48:03 +0200

    Adjust scroll padding bottom when keyboard appears

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:jangalma/helpers/utils.dart';
 import 'package:jangalma/screens/login_screen/viewmodels/login_viewmodel.dart';
 import 'package:jangalma/widgets/button_widget.dart';
 import 'package:jangalma/widgets/text_field_widget.dart';
@@ -22,70 +21,73 @@ class LogInScreen extends HookWidget {
     useEffect((){},[viewmodel.email, viewmodel.password]);
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          TextWidget(
-            text: 'Login',
-            type: 'headlineLarge',
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height*.5,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFieldWidget(
-                  currentText: viewmodel.email,
-                  label: "E-Mail",
-                  hintText: "Enter an email",
-                  type: 'email',
-                  onChanged: viewmodel.setEmail,
-                  isActive:  viewmodel.email.isNotEmpty,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height*.02),
-                TextFieldWidget(
-                  currentText: viewmodel.password,
-                  label: "Password",
-                  hintText: "Enter your password",
-                  type: 'password',
-                  onChanged: viewmodel.setPassword,
-                  isActive: viewmodel.password.isNotEmpty,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height*.02),
-                ButtonWidget(
-                  type: viewmodel.isFormValid() ? 'enabled' : 'default',
-                  disabledBackgroundColor: Theme.of(context).colorScheme.secondary,
-                  enabledBackgroundColor: Theme.of(context).colorScheme.primary,
-                  disabledTextColor: Colors.grey.shade300,
-                  title: "Login",
-                  action: ()=>{
-                    if(viewmodel.isFormValid()) {
-                      GoRouter.of(context).go('/home')
-                    }
-                  }
-                )
-              ],
-            ),
-          ),
-          Column(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextWidget(
-                text: "Don't have an account yet?",
+                text: 'Login',
+                type: 'headlineLarge',
                 color: Theme.of(context).colorScheme.primary,
               ),
-              GestureDetector(
-                onTap: () => GoRouter.of(context).go('/signup'),
-                child: TextWidget(
-                  text: "Sign up",
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              TextFieldWidget(
+                currentText: viewmodel.email,
+                label: "E-Mail",
+                hintText: "Enter an email",
+                type: 'email',
+                onChanged: viewmodel.setEmail,
+                isActive:  viewmodel.email.isNotEmpty,
               ),
+              SizedBox(height: MediaQuery.of(context).size.height*.02),
+              TextFieldWidget(
+                currentText: viewmodel.password,
+                label: "Password",
+                hintText: "Enter your password",
+                type: 'password',
+                onChanged: viewmodel.setPassword,
+                isActive: viewmodel.password.isNotEmpty,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height*.02),
+              ButtonWidget(
+                type: viewmodel.isFormValid() ? 'enabled' : 'default',
+                disabledBackgroundColor: Theme.of(context).colorScheme.secondary,
+                enabledBackgroundColor: Theme.of(context).colorScheme.primary,
+                disabledTextColor: Colors.grey.shade300,
+                title: "Login",
+                action: ()=>{
+                  if(viewmodel.isFormValid()) {
+                    GoRouter.of(context).go('/home')
+                  }
+                }
+              ),
+              bottomAction(context)
             ],
-          )
-        ],
+          ),
+        ),
       )
+    );
+  }
+
+  Row bottomAction(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextWidget(
+          text: "Don't have an account yet ?",
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        SizedBox(width: MediaQuery.of(context).size.width*.05),
+        GestureDetector(
+          onTap: () => GoRouter.of(context).go('/signup'),
+          child: TextWidget(
+            text: "Sign up",
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widgets/text_field_widget.dart
+++ lib/widgets/text_field_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:jangalma/helpers/utils.dart';
 
@@ -25,6 +27,9 @@ class TextFieldWidget extends StatelessWidget {
     Function validator = () => {};
     bool obscureText = false;
     TextInputType keyboardType;
+    double scrollPaddingBottom = Platform.isAndroid
+      ? MediaQuery.of(context).viewInsets.bottom+MediaQuery.of(context).size.height*.2
+      : MediaQuery.of(context).viewInsets.bottom+MediaQuery.of(context).size.height*.15;
 
     switch (type) {
       case "email":
@@ -52,7 +57,7 @@ class TextFieldWidget extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 8),
       width: MediaQuery.of(context).size.width*.9,
       child: TextFormField(
-        scrollPadding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom+MediaQuery.of(context).size.height*.5),
+        scrollPadding: EdgeInsets.only(bottom: scrollPaddingBottom),
         validator: (value) => validator(value),
         obscureText: obscureText,
         keyboardType: keyboardType,
